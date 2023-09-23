3d30e01b0f513198e3218989fea93673a8c0661f
Mahek <mahek.2002.singh@gmail.com>
2023-09-23 09:14:46 +0530

    first commit

---
+++ equilibriumPoint.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    int equilibriumPoint(long long a[], int n){
+         long long s = 0;
+        for(int i = 0; i < n; i++) s += a[i];
+        
+        long long left = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            if(i > 0) left += a[i-1];
+            long long right = s - left - a[i];
+            
+            if(right == left){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
