466c14cfb5aaa04bb9b070c8af0e450d0c656a90
Mahek <mahek.2002.singh@gmail.com>
2023-10-13 11:43:53 +0530

    13 October 2023 Medium

---
+++ floorInBST.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+private:
+    void fun(Node *root, int x, int &a)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        if (root->data <= x)
+        {
+            a = max(a, root->data);
+            fun(root->right, x, a);
+        }
+        else
+        {
+            fun(root->left, x, a);
+        }
+    }
+
+public:
+    int floor(Node *root, int x)
+    {
+        int a = -1;
+        fun(root, x, a);
+        return a;
+    }
+};
