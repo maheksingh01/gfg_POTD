80033c9ab573cafcf4f99b95ba594d8ef244ea52
Mahek <mahek.2002.singh@gmail.com>
2023-11-11 22:14:24 +0530

    11 November 2023 Easy

---
+++ isomorphicString.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    bool areIsomorphic(string str1, string str2)
+    {
+        if (str1.length() != str2.length())
+            return false;
+
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+
+        for (int i = 0; i < str1.length(); i++)
+        {
+            mp1[str1[i]] = str2[i];
+            mp2[str2[i]] = str1[i];
+        }
+
+        for (int i = 0; i < str1.length(); i++)
+        {
+            if (mp1[str1[i]] != str2[i])
+                return false;
+            if (mp2[str2[i]] != str1[i])
+                return false;
+        }
+
+        return true;
+    }
+};
