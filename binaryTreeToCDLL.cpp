4e7c6dff11a0484b5e5bcad0063b162cac53f342
Mahek <mahek.2002.singh@gmail.com>
2023-11-17 15:11:01 +0530

    17 November 2023 Medium

---
+++ binaryTreeToCDLL.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+#define append push_back
+    void dfs(Node *root, vector<int> &v)
+    {
+        if (root == nullptr)
+            return;
+        dfs(root->left, v);
+        v.append(root->data);
+        dfs(root->right, v);
+    }
+    Node *bTreeToCList(Node *root)
+    {
+        vector<int> v;
+        dfs(root, v);
+        Node *ptr = newNode(v[0]);
+        ptr->left = ptr, ptr->right = ptr;
+        Node *start = ptr;
+        for (int i = 1; i < v.size(); i++)
+        {
+            Node *next = newNode(v[i]);
+            next->left = ptr;
+            ptr->right = next;
+            next->right = next;
+            ptr = next;
+        }
+        start->left = ptr;
+        ptr->right = start;
+        return start;
+    }
+};
